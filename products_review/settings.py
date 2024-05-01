5770be319fa267bacc6f1f772b8509da38020a90
isaanita <isaanita30@gmail.com>
2024-05-01 13:24:38 +0700

    added banner and card products

--- products_review/settings.py
+++ products_review/settings.py
@@ -116,7 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# Lokasi di mana file media diunggah akan disimpan pada server
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# URL yang akan digunakan untuk mengakses file media
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
